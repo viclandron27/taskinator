cbd4d99ba070fafd834594b800e4b98118db2292
Victoria Landron <viclandron@gmail.com>
2020-08-30 19:54:58 -0400

    save edited tasks properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,16 +18,36 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
         formEl.reset();
         
-        //package up data as an object
-        var taskDataObj = {
-            name: taskNameInput,
-            type: taskTypeInput
-        };
+        var isEdit = formEl.hasAttribute("data-task-id");
 
-        //send it as an argument to createTaskEl
-        createTaskEl(taskDataObj);
-    
-    
+        //has data attribute, so get task id and call function to complete edit process
+        if (isEdit) {
+            var taskId = formEl.getAttribute("data-task-id");
+            completeEditTask(taskNameInput, taskTypeInput, taskId);
+        }
+        else {  
+            var taskDataObj = {
+                name: taskNameInput,
+                type: taskTypeInput
+            };
+
+            createTaskEl(taskDataObj);
+        }
+        
+    };
+
+    var completeEditTask = function(taskName, taskType, taskId) {
+        //find the matching task list item
+        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+        //set new values
+        taskSelected.querySelector("h3.task-name").textContent = taskName;
+        taskSelected.querySelector("span.task-type").textContent = taskType;
+
+        alert("Task Updated!");
+
+        formEl.removeAttribute("data-task-id");
+        document.querySelector("#save-task").textContent = "Add Task";
     };
 
     var createTaskEl = function(taskDataObj) {
