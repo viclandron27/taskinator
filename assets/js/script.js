26505d1976be99bfd3a15abd898edff919d38c04
Victoria Landron <viclandron@gmail.com>
2020-08-30 20:12:28 -0400

    moving tasks to the other categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var pageContentEl = document.querySelector("#page-content");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
     var taskFormHandler = function(event) {
 
         event.preventDefault();
@@ -146,6 +149,27 @@ var taskButtonHandler = function(event) {
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
+var taskStatusChangeHandler = function(event) {
+    //get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    //find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 var editTask = function(taskId) {
 
     //get task list item element
@@ -167,3 +191,5 @@ var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
 };
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
